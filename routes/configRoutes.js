01db0336d868f228a39292e1aa0a6a4c5e72c74f
omri <omrialter3@gmail.com>
2023-07-27 19:20:13 +0300

    s

--- routes/configRoutes.js
+++ routes/configRoutes.js
@@ -3,6 +3,7 @@ const usersR = require("./users");
 const userPostsR = require("./userPosts");
 const commentsR = require("./comments");
 const groupsR = require("./groups");
+const uploadR = require("./upload");
 
 exports.routesInit = (app) => {
   app.use("/", indexR);
@@ -10,6 +11,7 @@ exports.routesInit = (app) => {
   app.use("/userPosts", userPostsR);
   app.use("/comments", commentsR);
   app.use("/groups", groupsR);
+  app.use("/upload", uploadR);
 
   app.use("/*", (req, res) => {
     res.status(404).json({ msg: "page not found 404" })
